8ff0427797f08f2655a689c4be4fe9a9e1f91bda
JoanFerrero <joan.ferrero.montiel@gmail.com>
2023-10-11 19:18:31 +0200

    add login backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,20 +14,46 @@ exports.registerUser = asyncHandler(async (req, res) => {
     const userExist = await User.findOne({$or: [{ username: username }, { email: email }]});
 
     if(userExist) {
-        return res.status(400).json({
-            message: "User Exist"
+        return res.status(401).json({
+            message: "User exist"
         });
-    }
+    };
 
     const hashedPwd = await bcrypt.hash(password, 10);
 
     const newUser =  await User.create({
         username,
         email,
-        password,
+        password: hashedPwd,
     });
 
-    return res.status(400).json({
-        user: await userExist.toUserResponse()
+    return res.status(200).json({
+        user: await newUser.toUserResponse()
+    });
+});
+
+exports.loginUser = asyncHandler(async (req, res) => {
+    const { email, password} = req.body;
+
+    if(!email || !password) {
+        return req.status(400).json({
+            message: "All data is required"
+        });
+    };
+
+    const userExist = await User.findOne({ email: email });
+
+    if(!userExist) {
+        return res.status(401).json({
+            message: "User not exist"
+        });
+    };
+
+    const match = await bcrypt.compare(password, userExist.password);
+
+    if (!match) return res.status(401).json({ message: 'Unauthorized: Wrong password' });
+
+    res.status(200).json({
+        user: userExist.toUserResponse()
     });
 });
